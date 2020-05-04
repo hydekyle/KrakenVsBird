c65334989baac58ccb27d2981073f6c02bd8d561
Hyde_Wick <hydekyle@hotmail.com>
2020-05-04 16:45:22 +0100

    ok

---
+++ AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    public AudioSource audioSource_speakers;
+    Animator animator_speakers;
+
+    private void Start()
+    {
+        animator_speakers = audioSource_speakers?.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F8)) StartMusic();
+    }
+
+    public void StartMusic()
+    {
+        animator_speakers?.Play("moveUp");
+        audioSource_speakers?.Play();
+        CamaraManager.Instance.SetCameraMode(CamaraMode.Rave);
+    }
+}
---
+++ CamaraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CamaraMode { Idle, Rave }
+public class CamaraManager : MonoBehaviour
+{
+    public static CamaraManager Instance;
+    public Camera mainCamera;
+
+    private void Start()
+    {
+        Instance = Instance ?? this;
+        mainCamera = Camera.main;
+    }
+
+    public void SetCameraMode(CamaraMode mode)
+    {
+        switch (mode)
+        {
+            case CamaraMode.Rave: mainCamera.GetComponent<Animator>().Play("MusicOn"); break;
+        }
+    }
+}
